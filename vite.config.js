e7067bdc3ca57c171bfa689111c4f868e2e6e425
Jehan Paula <jehan.paula22@gmail.com>
2026-04-29 15:16:52 +0530

    test3

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,10 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-<<<<<<< HEAD
-            input: ['resources/css/App.css',
-
-                'resources/js/app.js',
-                'resources/js/admin.js'
-                // 'resources/css/userRegStyles.css'
-=======
             input: [
                 'resources/css/App.css',
-                'resources/js/app.js'
-
->>>>>>> 1f8edfc (added the logo)
-
-
-
+                'resources/js/app.js',
+                'resources/js/admin.js'
             ],
             refresh: true,
         }),
